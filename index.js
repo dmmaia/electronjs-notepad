526eed610be130a6fadeb052df7aa545d9e708de
dmmaia <david_mmaia@hotmail.com>
2024-08-14 11:32:48 -0300

    feat: preferences window created

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
+const path = require('path');
+
+let mainWIndow = null;
+let childWindow = null;
 
 const createWindow = () => {
-  const win = new BrowserWindow({
+  mainWIndow = new BrowserWindow({
     width: 800,
     height: 600
   })
-
-  win.loadFile('index.html')
+  mainWIndow.loadFile('templates/index.html')
 }
+async function createPrefWindow() {
+  childWindow = new BrowserWindow({
+    height: 300,
+    width: 300,
+    //show: false,
+    minimizable: false,
+    maximizable: false,
+    parent: mainWIndow,
+  })
+  childWindow.webContents.on('dom-ready', () => {
+    childWindow.show();
+  });
+  //configCapacitor(childWindow);
+  childWindow.removeMenu();
+  childWindow.loadURL(`file://${__dirname}/templates/preferences.html`);
+}
+
+const template = [
+  {
+    label: "File",
+    submenu: [
+      { label: "save" }
+    ]
+  },
+  {
+    label: "Edit",
+    submenu: [
+      {
+        label: "preferences",
+        accelerator: "F1",
+        click: async () => {
+          createPrefWindow();
+        }
+      }
+    ]
+  }
+]
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
+
 app.whenReady().then(() => {
   createWindow()
 })
